645f1f6f5cc02dc8fff30394fb34b5b03b5e5dac
RotKai <89800981+RotKai@users.noreply.github.com>
2022-09-27 12:36:40 -0700

    Create 794. Valid Tic-Tac-Toe State.cpp

---
+++ array/794. Valid Tic-Tac-Toe State.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool validTicTacToe(vector<string>& board) {
+        int x_count = 0, o_count = 0;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (board[i][j] == 'X')
+                    x_count += 1;
+                if (board[i][j] == 'O')
+                    o_count += 1;
+            }
+        }
+        // 数量不满足
+        if (x_count != o_count && x_count - 1 != o_count)
+            return false;
+        // X赢，但数量不满足条件
+        if (check(board, 'X') && x_count - 1 != o_count)
+            return false;
+         // O赢，但数量不满足条件
+        if (check(board, 'O') && x_count != o_count)
+            return false;
+
+        return true;
+    }
+
+    bool check(vector<string>& board, char p) {
+        for (int i = 0; i < 3; ++i) {
+            // 每一行
+            if (p == board[i][0] && p == board[i][1] && p == board[i][2])
+                return true;
+            // 每一列
+            if (p == board[0][i] && p == board[1][i] && p == board[2][i])
+                return true;
+        }
+        // 两个对角线
+        if (p == board[0][0] && p == board[1][1] && p == board[2][2])
+            return true;
+        if (p == board[0][2] && p == board[1][1] && p == board[2][0])
+            return true;
+        return false;
+    }
+};
+
